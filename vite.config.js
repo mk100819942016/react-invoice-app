3edc9af65acabcf672735e30096bfc83446c299c
mk100819942016 <mk100819942016@gmail.com>
2025-11-16 20:11:44 +0530

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",   // <-- ADD THIS LINE 
   plugins: [react()],
+  base: "/react-invoice-app/",   // <-- ADD THIS LINE 
 })
